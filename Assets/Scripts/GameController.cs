09d201cb5d0e98d6a425d1932ab114be7d1df7df
Peter Clotworthy <clotters@gmail.com>
2015-03-15 17:44:30 +0000

    Structural changes, a utility class and "bug" fixes

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	private int researchPoints;
+    private Text score;
+    public int researchPerClick = 10;
+
+
+	// Use this for initialization
+	void Start () {
+        GameObject scoreObject = GameObject.Find("Score");
+		researchPoints = 0;
+
+        score = scoreObject.GetComponent<Text>();
+
+        score.text = researchPoints.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonUp(0)) {
+            Utility.UnityLog(researchPerClick + " points added");
+            addResearchPoints(researchPerClick);
+        }
+		score.text = researchPoints.ToString();
+	}
+
+    void OnMouseUp() {
+        
+    }
+
+	public void addResearchPoints(int points){
+		researchPoints += points;
+	}
+}
