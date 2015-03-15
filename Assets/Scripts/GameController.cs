578646d7c201d0e72a7da990ecfe9305b0edee9e
Peter Clotworthy <clotters@gmail.com>
2015-03-15 21:18:17 +0000

    Score fixed and made GameController a singleton.
    GameController is now a singleton class, in that only one
    instance can exist at one time. You access this by calling it's static
    member "instance". E.g. GameController.instance.addResearchPoints().
    This makes sense as there should only be one object. Creating a new
    object will return the first instance without error, so GameController
    asd = GameObject.FindByTag("Camera").GetComponent<GameController>();
    will still work. I also deleted the default scene because it was pissing
    me off.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,22 +4,35 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
-	private int researchPoints;
-	public Text score;
+    // See http://unitypatterns.com/singletons/ for more details. Alternatively, google C# singleton.
+    private static GameController _instance;
+    public static GameController instance {
+        get {
+            if (_instance == null)
+                _instance = GameObject.FindObjectOfType<GameController>();
+            return _instance;
+        }
+    }
 
+	private int researchPoints = 150;
+	private Text score;
 
+    void Awake() {
+        score = GameObject.FindWithTag("GUI_Score").GetComponent<Text>();
+    }
 	// Use this for initialization
 	void Start () {
-		researchPoints = 0;
-		score.text = researchPoints.ToString();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		score.text = researchPoints.ToString();
+        score.text = researchPoints.ToString();
+        //Utility.UnityLog(researchPoints);
 	}
 
 	public void addResearchPoints(int points){
-		researchPoints += points;
+		this.researchPoints += points;
+        //score.text = this.researchPoints.ToString();
 	}
 }
