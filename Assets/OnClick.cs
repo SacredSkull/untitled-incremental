d8bb2d311a41b3e7dbe2ccc5bb146090b6c30cf8
Hanso38 <clogan38@live.co.uk>
2015-03-15 15:35:14 +0000

    Added non functioning changes
    Click to change points. This should update and display your new amount
    of points, none of this is happening, I am going to take a break for a
    while, but if you can find the issue...

---
+++ Assets/OnClick.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnClick : MonoBehaviour {
+
+	public int researchPerClick = 10;
+	private GameController gameController;
+
+	// Use this for initialization
+	void Start () {
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) {
+			gameController = gameControllerObject.GetComponent<GameController>();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		gameController.addResearchPoints (researchPerClick);
+	}
+}
