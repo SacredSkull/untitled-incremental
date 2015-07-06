c486708ef82bc61f1705a3990e2e1d6643e44be6
Conal Logan <clogan38@live.co.uk>
2015-07-06 21:25:27 +0100

    Research ordered by most recently discovered+ other features
    Research window now has 5 buttons
    Lots of new algorithms.
    Research window will now dissappear when research is being done, a
    placeholder shows instead

---
+++ Assets/Scripts/Research Browser/ClickR5.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickR5 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnMouseDown(){
+		GameController game = GameController.instance;
+		int ID = GameObject.Find("r5").GetComponent<ResearchID>().ID;
+		if (game.AllUncompleteResearch.ContainsKey (ID)) {
+			game.startResearch(game.AllUncompleteResearch[ID]);
+		}
+	}
+}
