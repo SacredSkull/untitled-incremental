f9dade9c654d04525457976673168de864233f3f
Conal Logan <clogan38@live.co.uk>
2015-07-07 16:23:32 +0100

    Research Picker -->Project Picker
    You can now use the menu to buy parts and build hardware and software.
    Unfortunately this comes with some bugs, but at least we know they exist
    now.

---
+++ Assets/Scripts/Work Browser/ClickR2.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickR2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnMouseDown(){
+		GameController game = GameController.instance;
+		int ID = GameObject.Find("r2").GetComponent<WorkID>().ID;
+		if (game.chapter == GameController.pickedType.Research) {
+			if (game.AllUncompleteResearch.ContainsKey (ID)) {
+				game.startResearch (game.AllUncompleteResearch [ID]);
+			}
+		}else if (game.chapter == GameController.pickedType.Software) {
+			if (game.UnstartedSoftware.ContainsKey (ID)) {
+				game.startSoftware (game.UnstartedSoftware [ID]);
+			}
+		} else if (game.chapter == GameController.pickedType.Hardware) {
+			if (game.UnstartedHardware.ContainsKey (ID)) {
+				game.makeHardware (game.UnstartedHardware [ID]);
+			}
+		} else if (game.chapter == GameController.pickedType.Parts) {
+			if (game.allBuyableParts.ContainsKey (ID)) {
+				game.buyPart (game.allBuyableParts [ID],1);
+			}
+		} else {
+			game.setChapterToSoftware();
+		}
+	}
+}
