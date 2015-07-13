b68918d777699de0e9bf576e9d54e85985c48811
Conal Logan <clogan38@live.co.uk>
2015-07-13 18:43:00 +0100

    Hardware and Software Types
    Both have been added to the databse, implementation has begun on
    Hardware

--- Assets/Scripts/HardwareProject.cs
+++ Assets/Scripts/HardwareProject.cs
@@ -8,6 +8,12 @@ using Incremental.Database;
 using UnityEngine;
 
 public class HardwareProject : Project {
+	public enum type
+	{
+		Computer,
+		None
+	}
+
     private ICollection<Part> _Parts;
 
     public ICollection<Part> Parts {
@@ -23,11 +29,35 @@ public class HardwareProject : Project {
         }
     }
 
+	public type _HardwareType;
+
+	public type HardwareType{
+		get{
+			if(_HardwareType == null){
+				String parseable;
+				using (DatabaseConnection conn = new DatabaseConnection()){
+					const string sql = @"SELECT t.Name name FROM SoftwareProjectType as t INNER JOIN( SELECT hwr.TypeID, hwr.HardwareProjectID FROM HardwareProject_Type as hwr WHERE hwr.HardwareProjectID = @PID)as hwr ON hwr.TypeID = t.ID;";
+					parseable = conn.connection.Query<String>(sql, new { PID = ID }).ToString();
+
+				}
+				foreach (type item in Enum.GetValues(typeof(type))){
+					if(item.ToString().Equals(parseable)){
+						_HardwareType = item;
+						return _HardwareType;
+					}
+				}
+				_HardwareType = type.None;
+			}
+			return _HardwareType;
+		}
+	}
+
 	//This used to give a list of Parts and research required, no idea why
 	//Changing it for now, as it bogs the method down significantly, while
 	//the info will come in handy I don't thin it is necessary for this method.
     public bool possible()
     {
+		Debug.Log (this.HardwareType.ToString ()+"  " +name);
         foreach (Research r in this.Research) {
             if (!r.hasBeenDone()) {
 				return false;
