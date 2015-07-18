495ed14c2bfa48013d7990bbf9e0a3d652de9991
Peter Clotworthy <clotters@gmail.com>
2015-07-19 00:56:18 +0100

    Added the `Asset` class, which Startable inherits. It contains the very most basic of information, such as the name and ID.

---
+++ Assets/Scripts/Asset.cs
@@ -0,0 +1,21 @@
+﻿/**
+ * @class   Asset
+ *
+ * @brief   Assets represent anything in the game that may be required to be displayed or used in some way
+ *
+ * @author  Peter
+ * @date    20/03/2015
+ * @updated 19/07/2015
+ */
+
+public class Asset {
+    public virtual string name {
+        get;
+        set;
+    }
+
+    public virtual int ID {
+        get;
+        set;
+    }
+}
--- Assets/Scripts/Startable.cs
+++ Assets/Scripts/Startable.cs
@@ -6,27 +6,17 @@ using System.Text;
 /**
  * @class   Startable
  *
- * @brief   This class is to be inherited by anything that is 'missingRequirements' - Research or projects, for example.
+ * @brief   This is the base class for anything that is startable, completeable or abandonable.
  *
  * @author  Peter
  * @date    20/03/2015
+ * @updated 19/07/2015
  */
 
-public abstract class Startable {
+public abstract class Startable : Asset {
     public abstract void complete();
     public abstract void abandon();
 
     // \todo Is this method necessary?
     public abstract void start();
-
-    public virtual string name {
-        get;
-        set;
-    }
-
-    public virtual int number
-    {
-        get;
-        set;
-    }
 }
