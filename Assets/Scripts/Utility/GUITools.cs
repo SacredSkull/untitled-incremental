53f52c9d4eb4ead43beb669a22b95c283fe843ee
Peter Clotworthy <clotters@gmail.com>
2015-07-19 03:05:33 +0100

    Added the simplified base class for Startable which only contains ID & name currently. Added randomised properties for the name and field of employees and vastly improved the readibility of the class by making many of the fields doubles rather than integers (which will also prevent data loss from the conversion that was occuring).

--- Assets/Scripts/Utility/GUITools.cs
+++ Assets/Scripts/Utility/GUITools.cs
@@ -10,9 +10,9 @@ public static class GUITools  {
 	public static void setButtonVisible(String name,bool visible){
 		GameObject button = GameObject.Find(name);
 		if (!visible) {
-			button.GetComponent<CanvasGroup> ().alpha = 0;
-			button.GetComponent<Button> ().interactable = false;
-			button.GetComponent<CanvasGroup> ().interactable = false;
+			button.GetComponent<CanvasGroup>().alpha = 0;
+			button.GetComponent<Button>().interactable = false;
+			button.GetComponent<CanvasGroup>().interactable = false;
 		} else {
 			button.GetComponent<CanvasGroup>().alpha = 1;
 			button.GetComponent<Button>().interactable = true;
@@ -23,12 +23,7 @@ public static class GUITools  {
 	//warning, only use this method on stored gameObjects, if a Gameobject is 
 	//deactivated simply by finding it, it cannot be found again to reactivate, store the GameObject in the 
 	//GameController class.
-	public static void setGameObjectActive(GameObject g, bool active){
-		if (active) {
-			g.active = true;
-		} else {
-			g.active = false;
-		}
+	public static void setGameObjectActive(GameObject g, bool active) {
+	    g.SetActive(active);
 	}
-
 }
